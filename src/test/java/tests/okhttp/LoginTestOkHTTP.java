8ac62839ca730f97b4711f186002166740cbe13d
julia <juliiagordin@gmail.com>
2023-11-17 11:59:15 +0300

    add dto for api okhttp login test

---
+++ src/test/java/tests/okhttp/LoginTestOkHTTP.java
@@ -0,0 +1,57 @@
+package tests.okhttp;
+
+import com.google.gson.Gson;
+import dto.AuthResponseDTO;
+import dto.UserDtoLombok;
+import okhttp3.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class LoginTestOkHTTP {
+
+    UserDtoLombok user = UserDtoLombok.builder()
+            .username("testqa20@gmail.com")
+            .password("123456Aa$")
+            .build();
+
+    public static final MediaType JSON = MediaType.get("application/json");
+    Gson gson = new Gson();
+    OkHttpClient okHttpClient = new OkHttpClient();
+    String baseUrl = "https://contactapp-telran-backend.herokuapp.com";
+
+    @Test
+    public void loginPositive() {
+        RequestBody requestBody = RequestBody.create(gson.toJson(user), JSON);
+        Request request = new Request.Builder()
+                .url(baseUrl + "/v1/user/login/usernamepassword")
+                .post(requestBody)
+                .build();
+        Response response;
+        try {
+            response = okHttpClient.newCall(request).execute();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        if(response == null) {
+            Assert.fail("got null response");
+        } else if (response.isSuccessful()) { //  return OK 200 and token
+            String responseJson;
+            try {
+                responseJson = response.body().string();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            AuthResponseDTO authResponseDTO = gson.fromJson(responseJson, AuthResponseDTO.class);
+            System.out.println(authResponseDTO.getToken());
+            System.out.println(response.code());
+            System.out.println(response.message());
+            Assert.assertEquals(response.code(), 200, "response not 200");
+        } else {
+            System.out.println(response.code() + " error");
+            Assert.fail("response not successfully");
+        }
+    }
+}
